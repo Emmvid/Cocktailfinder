f9ba4d5c8f7b318a6325a156f18d3b36efc0c2bd
Emmvid <emmavidarsson@hotmail.com>
2022-11-24 15:34:30 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'> 
+      <h1 className='section-title'> About us
+      </h1>
+        <p> 
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur recusandae nihil, amet temporibus velit nulla veritatis sed praesentium totam non?
+        </p>
+    </section>
+  )
+}
+
+export default About
